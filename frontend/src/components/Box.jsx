d494e4ea8a51863afa40ba4e982d37e13ba56636
Hunt Tynch <tynchhunt@gmail.com>
2025-02-16 00:30:11 -0500

    Finished overview layout, need to connect back.

--- frontend/src/components/Box.jsx
+++ frontend/src/components/Box.jsx
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 
 function Box({ children, params }) {
   return (
-    <div className={`flex flex-col items-start bg-white rounded-lg shadow shadow-gray-500 px-4 ${params}`}>
+    <div className={`flex flex-col items-start rounded-lg shadow shadow-gray-500 px-4 ${params}`}>
       {children}
     </div>
   );
--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -10,8 +10,8 @@ function Home() {
   };
 
   return (
-    <div className='grid grid-cols-2 text-black ml-[276px] mr-4 h-[calc(100vh-1rem)] my-2 relative'>
-      <Box params='col-span-2 mb-2 pt-5 px-6'>
+    <div className='grid grid-cols-2 text-black ml-[276px] mr-4 min-h-[calc(100vh-1rem)] my-2 relative'>
+      <Box params='col-span-2 pt-5 px-6 bg-white min-h-[calc(50vh-1rem)] max-h-[calc(50vh-1rem)]'>
         <div className='w-full flex items-center border-b-2 border-gray-300 pb-1'>
           <p className='text-4xl font-bold'>Hunt Overview</p>
           <div className='flex ml-auto items-center'>
@@ -28,18 +28,148 @@ function Home() {
             </div>
           </div>
         </div>
-        <div>
+        <div className='flex flex-wrap min-h-[calc(100%-4px-36px-4px-20px)] justify-between w-full'>
+          <Box params='my-auto bg-slate-50 w-[calc(50%-1rem)] h-[calc(50%-2rem)]'>
+            <div className='w-full h-full flex flex-col items-center'>
+              <p className='text-start text-2xl font-medium pt-2 w-full border-b-2 border-gray-300'>Title</p>
+              <p className='flex justify-center w-full my-auto items-center text-2xl font-bold'>Masters Fox Hunt Trial</p>
+            </div>
+          </Box>
+          <Box params='my-auto bg-slate-50 w-[calc(50%-1rem)] h-[calc(50%-2rem)]'>
+            <div className='w-full h-full flex flex-col items-center'>
+              <p className='text-start text-2xl font-medium pt-2 w-full border-b-2 border-gray-300'>Date</p>
+              <p className='flex justify-center w-full my-auto items-center text-2xl font-bold'>01-16-2025 to 01-18-2025</p>
+            </div>
+          </Box>
+          <Box params='my-auto bg-slate-50 w-[calc(50%-1rem)] h-[calc(50%-2rem)]'>
+            <div className='w-full h-full flex flex-col items-center'>
+              <p className='text-start text-2xl font-medium pt-2 w-full border-b-2 border-gray-300'>Stake</p>
+              <p className='flex justify-center w-full my-auto items-center text-2xl font-bold'>All Age</p>
+            </div>
+          </Box>
+          <Box params='my-auto bg-slate-50 w-[calc(50%-1rem)] h-[calc(50%-2rem)]'>
+            <div className='w-full h-full flex flex-col items-center'>
+              <p className='text-start text-2xl font-medium pt-2 w-full border-b-2 border-gray-300'>Interval</p>
+              <p className='flex justify-center w-full my-auto items-center text-2xl font-bold'>10</p>
+            </div>
+          </Box>
         </div>
       </Box>
-      <Box params='col-span-1 mt-2 mr-2 pt-5 px-6'>
+      <Box params='col-span-1 mt-2 mr-2 pt-5 px-6 bg-white max-h-[calc(50vh-1rem)] min-h-[calc(50vh-1rem)]'>
         <div className='w-full flex border-b-2 border-gray-300 items-center pb-1'>
           <p className='text-3xl font-bold'>Top Dogs</p>
         </div>
+        <Box params='overflow-y-auto w-full p-4 my-8 bg-slate-50 h-full'>
+          <table className='table-auto w-full h-full'>
+            <thead>
+              <tr className='border-b-2 border-gray-300'>
+                <th className='text-xl font-semibold text-start'>#</th>
+                <th className='text-xl font-semibold text-start'>Name</th>
+                <th className='text-xl font-semibold text-end'>Points</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td className='text-xl text-start'>1</td>
+                <td className='text-xl text-start'>Rex</td>
+                <td className='text-xl text-end'>100</td>
+              </tr>
+              <tr>
+                <td className='text-xl text-start'>1</td>
+                <td className='text-xl text-start'>Rex</td>
+                <td className='text-xl text-end'>100</td>
+              </tr>
+              <tr>
+                <td className='text-xl text-start'>1</td>
+                <td className='text-xl text-start'>Rex</td>
+                <td className='text-xl text-end'>100</td>
+              </tr>
+              <tr>
+                <td className='text-xl text-start'>1</td>
+                <td className='text-xl text-start'>Rex</td>
+                <td className='text-xl text-end'>100</td>
+              </tr>
+              <tr>
+                <td className='text-xl text-start'>1</td>
+                <td className='text-xl text-start'>Rex</td>
+                <td className='text-xl text-end'>100</td>
+              </tr>
+              <tr>
+                <td className='text-xl text-start'>1</td>
+                <td className='text-xl text-start'>Rex</td>
+                <td className='text-xl text-end'>100</td>
+              </tr>
+            </tbody>
+          </table>
+        </Box>
       </Box>
-      <Box params='col-span-1 mt-2 ml-2 pt-5 px-6'>
+      <Box params='col-span-1 mt-2 ml-2 pt-5 px-6 bg-white max-h-[calc(50vh-1rem)] min-h-[calc(50vh-1rem)]'>
         <div className='w-full flex border-b-2 border-gray-300 items-center pb-1'>
-          <p className='text-3xl font-bold'>Today&apos;s Frontrunners</p>
+          <p className='text-3xl font-bold'>Daily 10</p>
         </div>
+        <Box params='overflow-y-auto w-full p-4 my-8 bg-slate-50 h-full'>
+          <table className='table-auto w-full h-full'>
+            <thead>
+              <tr className='border-b-2 border-gray-300'>
+                <th className='text-xl font-semibold text-start'>#</th>
+                <th className='text-xl font-semibold text-start'>Name</th>
+                <th className='text-xl font-semibold text-end'>Points</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td className='text-xl text-start'>1</td>
+                <td className='text-xl text-start'>Rex</td>
+                <td className='text-xl text-end'>100</td>
+              </tr>
+              <tr>
+                <td className='text-xl text-start'>1</td>
+                <td className='text-xl text-start'>Rex</td>
+                <td className='text-xl text-end'>100</td>
+              </tr>
+              <tr>
+                <td className='text-xl text-start'>1</td>
+                <td className='text-xl text-start'>Rex</td>
+                <td className='text-xl text-end'>100</td>
+              </tr>
+              <tr>
+                <td className='text-xl text-start'>1</td>
+                <td className='text-xl text-start'>Rex</td>
+                <td className='text-xl text-end'>100</td>
+              </tr>
+              <tr>
+                <td className='text-xl text-start'>1</td>
+                <td className='text-xl text-start'>Rex</td>
+                <td className='text-xl text-end'>100</td>
+              </tr>
+              <tr>
+                <td className='text-xl text-start'>1</td>
+                <td className='text-xl text-start'>Rex</td>
+                <td className='text-xl text-end'>100</td>
+              </tr>
+              <tr>
+                <td className='text-xl text-start'>1</td>
+                <td className='text-xl text-start'>Rex</td>
+                <td className='text-xl text-end'>100</td>
+              </tr>
+              <tr>
+                <td className='text-xl text-start'>1</td>
+                <td className='text-xl text-start'>Rex</td>
+                <td className='text-xl text-end'>100</td>
+              </tr>
+              <tr>
+                <td className='text-xl text-start'>1</td>
+                <td className='text-xl text-start'>Rex</td>
+                <td className='text-xl text-end'>100</td>
+              </tr>
+              <tr>
+                <td className='text-xl text-start'>1</td>
+                <td className='text-xl text-start'>Rex</td>
+                <td className='text-xl text-end'>100</td>
+              </tr>
+            </tbody>
+          </table>
+        </Box>
       </Box>
 
       {showOverlay && (
@@ -53,7 +183,7 @@ function Home() {
               Edit Current Hunt
             </button>
             <button 
-              className='mt-4 text-red-500 hover:text-red-400 font-bold w-full cursor-pointer'
+              className='mt-4 bg-red-50 rounded text-red-500 hover:text-red-400 font-bold w-full cursor-pointer'
               onClick={handleOverlay}
             >
               Close
