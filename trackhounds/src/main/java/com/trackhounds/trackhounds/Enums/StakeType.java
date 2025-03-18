9ec32f0002659f8e146c7587a2cef13a5f9832e8
Hunt Tynch <tynchhunt@gmail.com>
2025-03-17 22:38:24 -0400

    Removed logic.

--- trackhounds/src/main/java/com/trackhounds/trackhounds/Enums/StakeType.java
+++
@@ -1,9 +0,0 @@
-package com.trackhounds.trackhounds.Enums;
-
-/**
- * Enum for the different types of stakes.
- */
-public enum StakeType {
-    ALL_AGE,
-    DERBY
-}
--- trackhounds/src/main/java/com/trackhounds/trackhounds/Repository/ScratchRepository.java
+++
@@ -1,9 +0,0 @@
-package com.trackhounds.trackhounds.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.trackhounds.trackhounds.Entity.Scratch;
-
-public interface ScratchRepository extends JpaRepository<Scratch, Long> {
-
-}
